10cf70291ddfe58d0ce614e70bb35165e5db0f2d
cptran777 <cptran14@gmail.com>
2016-10-24 01:02:58 -0700

    added project links and related nested routes

--- app/router.js
+++ app/router.js
@@ -7,7 +7,14 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('portfolio');
+  this.route('portfolio', function() {
+    this.route('avalon');
+    this.route('reeltime');
+    this.route('labit');
+    this.route('syrup');
+    this.route('music');
+    this.route('recast');
+  });
   this.route('interests');
 });
 
