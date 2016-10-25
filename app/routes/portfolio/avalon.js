31f7ead5f8780d1029eb82edb1f126590c5f9fc2
cptran777 <cptran14@gmail.com>
2016-10-24 22:22:33 -0700

    add base route to quickly extend scrolling to other routes. and scrolls to top now when changing routes

--- app/routes/portfolio/avalon.js
+++ app/routes/portfolio/avalon.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+		didTransition() {
+			let mainWindow = document.getElementById('main');
+			if (mainWindow) {
+				mainWindow.scrollTop = 0;			
+			}
+		}
+	}
 });
--- app/routes/portfolio/labit.js
+++ app/routes/portfolio/labit.js
@@ -1,4 +1,4 @@
 import Ember from 'ember';
+import base from '../base';
 
-export default Ember.Route.extend({
-});
+export default base;
